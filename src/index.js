ea8f4dcdea87f81758487f0fecd6620d1fd5b2fc
macrilege <18197988+macrilege@users.noreply.github.com>
2019-11-11 09:18:05 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,60 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import answerData from "./Data";
-import Magic8Ball from "./Magic8Ball";
-//import "./styles.css";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom'
+import { CSSTransition } from 'react-transition-group'
+import { Container, Navbar, Nav } from 'react-bootstrap'
+import Home from './pages/home'
+import About from './pages/about'
+import Contact from './pages/contact'
+import './styles.css'
 
-function App() {
+const routes = [
+  { path: '/', name: 'Home', Component: Home },
+  { path: '/about', name: 'About', Component: About },
+  { path: '/contact', name: 'Contact', Component: Contact },
+]
+
+function Example() {
   return (
-    <div className="App">
-      <Magic8Ball answerData={answerData} />
-    </div>
-  );
+    <Router>
+      <>
+        <Navbar bg="light">
+          <Nav className="mx-auto">
+            {routes.map(route => (
+              <Nav.Link
+                key={route.path}
+                as={NavLink}
+                to={route.path}
+                activeClassName="active"
+                exact
+              >
+                {route.name}
+              </Nav.Link>
+            ))}
+          </Nav>
+        </Navbar>
+        <Container className="container">
+          {routes.map(({ path, Component }) => (
+            <Route key={path} exact path={path}>
+              {({ match }) => (
+                <CSSTransition
+                  in={match != null}
+                  timeout={300}
+                  classNames="page"
+                  unmountOnExit
+                >
+                  <div className="page">
+                    <Component />
+                  </div>
+                </CSSTransition>
+              )}
+            </Route>
+          ))}
+        </Container>
+      </>
+    </Router>
+  )
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById('root')
+ReactDOM.render(<Example />, rootElement)
